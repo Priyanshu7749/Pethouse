8808e17749126f04a231c57816d6b930f166c0f2
priyanshu jethanandani <100209324+Priyanshu7749@users.noreply.github.com>
2024-11-07 21:35:29 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 // Your Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCjmU-legdt02b06NIc5-ocu2cn6vEqcNY",
-    authDomain: "mycp3-b19ee.firebaseapp.com",
-    projectId: "mycp3-b19ee",
-    storageBucket: "mycp3-b19ee.appspot.com",
-    messagingSenderId: "110805225847",
-    appId: "1:110805225847:web:ab8afcde9e189c50765172"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_ID,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APPID
   };
 
 // Initialize Firebase
